ea96fe33ede35663d3cc2b444e7111ac89ac53ad
Rahul De <rahul080327@gmail.com>
2020-04-11 11:36:27 +0200

    new beginnings

---
+++ gatsby-config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  siteMetadata: {
+    title: `Simplicity by Design`,
+    name: `lispyclouds`,
+    siteUrl: `https://lispyclouds.de`,
+    description: `Perspectives on empathy, sustainable living, technology, and simplicity`,
+    hero: {
+      heading: `Simple and Sustainable perspectives.`,
+      maxWidth: 652,
+    },
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/lispyclouds`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/lispyclouds`,
+      },
+      {
+        name: `linkedin`,
+        url: `https://www.linkedin.com/in/lispyclouds`,
+      },
+    ],
+  },
+  plugins: ['@narative/gatsby-theme-novela']
+};
